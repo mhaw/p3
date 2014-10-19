d8167f345ac9eadb2196738b52c87ac888f4e907
Mike Haw <mikehaw@gmail.com>
2014-10-19 16:06:54 -0400

    update page design and formatting

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -15,17 +15,93 @@
 </head>
 <body>
 
-	<h3>Mike's Dev ToolKit</h3>
+	    <!-- Static navbar -->
+    <div class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="http://p3.mikehaw.me">Mike's Dev Toolkit</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="http://p1.mikehaw.me">P1</a></li>
+            <li><a href="http://p2.mikehaw.me">P2</a></li>
+            <li><a href="http://p3.mikehaw.me">P3</a></li>
+            <li><a href="http://p4.mikehaw.me">P4</a></li>
+            <li><a href="http://dwa15.com/">DWA15</a></li>
+            <li><a href="https://github.com/mhaw/p3">P3 GibHub</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
 
-	@yield('intro')
+	<!-- +++++ Welcome Section +++++ -->
+	<div id="ww">
+	    <div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 centered">
+					<img src="assets/toolbox.png" alt="toolbox">
+					<h2>Welcome to Mike's Developer ToolKit!</h1>
+					<p>I created some simple tools that will assist</p>
+					<p>your web development tasks by generating sample</p>
+					<p>placeholder text, sample users, and strong passwords.</p>
+					<p>I hope you find these tools helpful!</p>
+				</div><!-- /col-lg-8 -->
+			</div><!-- /row -->
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 centered">
+					@yield('lorem')
+				</div><!-- /col-lg-8 -->
+			</div><!-- /row -->
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 centered">
+					@yield('users')
+				</div><!-- /col-lg-8 -->
+			</div><!-- /row -->
+	    </div> <!-- /container -->
+	</div><!-- /ww -->
+
 	
-	@yield('lorem')
+	
+	<!-- +++++ Footer Section +++++ -->
+	
+	<div id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4">
+					<p>
+						Made in<br/>
+						Durham, NC<br/>
 
-	@yield('users')
+					</p>
+				</div><!-- /col-lg-4 -->
+				
+				<div class="col-lg-4">
+					<h4>My Links</h4>
+					<p>
+						<a href="#">GitHub</a><br/>
+						<a href="#">Twitter</a><br/>
+						<a href="#">Facebook</a>
+					</p>
+				</div><!-- /col-lg-4 -->
+				
+				<div class="col-lg-4">
+				</div><!-- /col-lg-4 -->
+			
+			</div>
+		
+		</div>
+	</div>
+	
 
-	@yield('pass')
-
-	@yield('body')
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/bootstrap.min.js"></script>
 	
 </body>
 </html>
